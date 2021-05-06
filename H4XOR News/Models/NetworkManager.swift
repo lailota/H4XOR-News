90161e89833b00e4c4ffab168bdb168a4014a069
Laila GH <laila.guzzon@gmail.com>
2021-05-06 17:41:24 -0300

    update files

--- H4XOR News/Models/NetworkManager.swift
+++ H4XOR News/Models/NetworkManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class NetworkManager: ObservableObject {
+    
+   @Published var posts = [Post]()
+    
+    func fetchData() {
+        if let url = URL(string: "x") {
+        let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
